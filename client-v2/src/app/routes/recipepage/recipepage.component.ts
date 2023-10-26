7e1c13f328135f85ead59d24b0d378a32767110b
inkvoiid <kalebdickson@live.com>
2023-10-26 15:10:22 +1300

    Recipe Text to Speech
    Added a speech synthesiser that works on mobile and desktop, added stop functionality.

--- client-v2/src/app/routes/recipepage/recipepage.component.ts
+++ client-v2/src/app/routes/recipepage/recipepage.component.ts
@@ -25,6 +25,12 @@ export class RecipepageComponent implements OnInit {
   ingredientsLeft: string[] = [];
   ingredientsRight: string[] = [];
 
+  private currentUtterance: SpeechSynthesisUtterance;
+  speechIngredientsParagraph: string = '';
+  speechInstructionsParagraph: string = '';
+  speakingIngredients: boolean = false;
+  speakingInstructions: boolean = false;
+
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
@@ -32,7 +38,13 @@ export class RecipepageComponent implements OnInit {
     protected auth: AuthService,
     private recipeService: RecipeService,
     private dialog: MatDialog
-  ) {}
+  ) {
+    this.currentUtterance = new SpeechSynthesisUtterance();
+    enum SpeakingWhat {
+      Ingredients,
+      Instructions,
+    }
+  }
 
   ngOnInit(): void {
     this.route.params.subscribe(
@@ -84,12 +96,38 @@ export class RecipepageComponent implements OnInit {
         this.ingredientsRight = this.recipe.ingredients.slice(
           halfIngredientsLength
         );
+
+        for (let i = 0; i < this.recipe.instructions.length; i++) {
+          this.speechInstructionsParagraph +=
+            'Step ' +
+            (i + 1) +
+            ': ' +
+            this.checkForCommaOrPeriod(this.recipe.instructions[i]) +
+            ' ';
+        }
+
+        for (let i = 0; i < this.recipe.ingredients.length; i++) {
+          this.speechIngredientsParagraph +=
+            this.checkForCommaOrPeriod(this.recipe.ingredients[i]) + ' ';
+        }
       },
       (error) => {
         this.recipe = null;
         this.loading = false;
       }
     );
+  }
+
+  // Method that checks to see if a string ends with comma or period, if it's a comma, swap it with a period and if it has neither, add a period
+  // This is to make the speech synthesis sound more natural
+  // Written by: GitHub Copilot
+  checkForCommaOrPeriod(sentence: string) {
+    if (sentence.endsWith(',')) {
+      sentence = sentence.replace(/.$/, '.');
+    } else if (!sentence.endsWith('.')) {
+      sentence += '.';
+    }
+    return sentence;
   }
 
   loadPlaceholderImage() {
@@ -114,5 +152,51 @@ export class RecipepageComponent implements OnInit {
     let dialogRef = this.dialog.open(RejectrecipemodalComponent, {
       data: { recipeIdToDelete: this.recipe.recipeId, redirect: true },
     });
+  }
+
+  speakIngredients() {
+    this.stopSpeaking();
+    this.speakingIngredients = true;
+    const utterance = new SpeechSynthesisUtterance(
+      this.speechIngredientsParagraph
+    );
+    utterance.lang = 'en-US';
+
+    this.currentUtterance = utterance;
+
+    const endListener = (event: Event) => {
+      this.speakingIngredients = false;
+      this.currentUtterance.removeEventListener('end', endListener);
+    };
+
+    this.currentUtterance.addEventListener('end', endListener);
+    speechSynthesis.speak(utterance);
+  }
+
+  speakInstructions() {
+    this.stopSpeaking();
+    this.speakingInstructions = true;
+    const utterance = new SpeechSynthesisUtterance(
+      this.speechInstructionsParagraph
+    );
+    utterance.lang = 'en-US';
+
+    this.currentUtterance = utterance;
+
+    const endListener = (event: Event) => {
+      this.speakingInstructions = false;
+      this.currentUtterance.removeEventListener('end', endListener);
+    };
+
+    this.currentUtterance.addEventListener('end', endListener);
+    speechSynthesis.speak(utterance);
+  }
+
+  stopSpeaking() {
+    speechSynthesis.cancel();
+    this.speakingIngredients = false;
+    this.speakingInstructions = false;
+
+    this.currentUtterance.removeEventListener('end', () => {});
   }
 }
