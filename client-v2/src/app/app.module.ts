c7b958a9bc91ec335e4e2677dfcb4263784da4c9
inkvoiid <kalebdickson@live.com>
2024-05-11 18:38:11 +1200

    Remove Material File Input
    Also removed this in the last commit of the same name.

--- client-v2/src/app/app.module.ts
+++ client-v2/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialFileInputModule } from 'ngx-material-file-input';
 
 // Material
 import { MatButtonModule } from '@angular/material/button';
@@ -91,7 +90,6 @@ import { LoadingComponent } from './partial/loading/loading.component';
         },
       },
     }),
-    MaterialFileInputModule,
     MatStepperModule,
     MatSelectModule,
     MatRadioModule,
